4d17538e48485e1a5d3d9faa86beed72936f21a1
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-01 12:06:54 +0530

    made changes to dockerfile and sandbox.js

--- backend/sandbox.js
+++ backend/sandbox.js
@@ -4,5 +4,18 @@ const path   = require('path');
 const docker = new Docker({socketPath:"/var/run/docker.sock"});
 
 async function runCode(language,code) {
-    const tempDir = path.resolve('./sandbox');
+    //Maintain a temporary folder named : ./sandbox:
+    const tempDir = path.resolve('./sandbox'); 
+    if (!fs.existsSync(tempDir)){
+        fs.mkdirSync(tempDir);
+    }
+    let fileName, image, cmd;
+    if (language === 'js') {
+        fileName = 'code.js';
+        image = "nodejs-sandbox";
+        cmd = ["node","/app/code.js"];
+    } else if (language === "python") {
+        fileName = 'code.py';
+        image = ""
+    }
 }
