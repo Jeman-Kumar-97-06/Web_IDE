a0b988e9e5d0a0078de50f1c03e2a675fb4ad18a
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-01 11:29:04 +0530

    made changes

--- backend/socket.js
+++
@@ -1,8 +0,0 @@
-const Docker = require('dockernode');
-const fs     = require('fs');
-const path   = require('path');
-const docker = new Docker({socketPath:"/var/run/docker.sock"});
-
-async function runCode(landuage,code) {
-    
-}
