ca871f1b568d0e078e876c88636ead3eb12acdb6
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-11 09:20:04 +0530

    made changes

--- backend/sandbox.js
+++ backend/sandbox.js
@@ -5,12 +5,14 @@ const path        = require('path');
 const docker      = new Docker({socketPath:"/var/run/docker.sock"});
 
 async function runCode(language,code) {
+    
     //Maintain a temporary folder named : ./sandbox:
     const tempDir = path.resolve('./sandbox'); 
     if (!fs.existsSync(tempDir)){
         fs.mkdirSync(tempDir);
     }
     let fileName, image, cmd;
+
     //Check the programming language of input:
     if (language === 'js') {
         fileName = 'code.js';
